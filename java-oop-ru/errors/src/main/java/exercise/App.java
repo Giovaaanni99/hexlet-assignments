e1cef0dc02187eb5683e65ab3473436259366e1d
@hexlet/cli <support@hexlet.io>
2025-03-04 15:36:18 +0300

    submit java-oop-ru/errors

---
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -0,0 +1,17 @@
+package exercise;
+
+// BEGIN
+class App {
+    public static void printSquare(Circle circle) {
+        try {
+            double area = circle.getSquare();
+            System.out.println((int)Math.round(area));
+        } catch (NegativeRadiusException e){
+            System.out.println("Не удалось посчитать площадь");
+        }finally {
+            System.out.println("Вычисление окончено");
+        }
+
+    }
+}
+// END
---
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -0,0 +1,26 @@
+package exercise;
+
+// BEGIN
+class Circle {
+    private Point center;
+    private int radius;
+
+    public Circle(Point center, int radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public double getSquare() throws NegativeRadiusException{
+        if(radius < 0) {
+            throw new NegativeRadiusException("Радиус не может быть отрицательным!");
+
+        }
+        double square = Math.PI * (radius*  radius);
+        return square;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
+// END
